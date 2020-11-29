51cb3e5468ac3afeaf6fe581a6ca78037b084a59
strelov1 <strelov1@gmail.com>
2020-11-29 13:41:03 +0300

    add linter and improve help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,9 @@
 import { program } from 'commander';
 
 program
-  .version('0.1.0');
+  .version('0.1.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .description(' Compares two configuration files and shows a difference.');
 
 program.parse(process.argv);
