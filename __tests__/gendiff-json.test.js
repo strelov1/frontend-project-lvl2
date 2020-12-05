6ecaa0777937ee1a2a41a83036cc035dbadafce2
strelov1 <strelov1@gmail.com>
2020-12-06 00:32:16 +0300

    add yaml support

--- __tests__/gendiff-json.test.js
+++ __tests__/gendiff-json.test.js
@@ -6,43 +6,49 @@ import genDiff from '../src/gendiff.js';
 const getFixitureFile = (fileName) => path.join('__tests__/__fixtures__/', fileName);
 
 test('compare file1.json file2.json', () => {
-  const diff = genDiff(getFixitureFile('file1.json'), getFixitureFile('file2.json'));
 
-  expect(diff).toEqual(`{
+  const expected = `{
     host: hexlet.io
   + verbose: true
   - proxy: 123.234.53.22
   - follow: false
   - timeout: 50
   + timeout: 20
-}`);
+}`
+
+  const diff = genDiff(getFixitureFile('file1.json'), getFixitureFile('file2.json'));
+  
+  expect(diff).toEqual(expected);
 });
 
 test('compare file1.json file1.json ', () => {
-  const diff = genDiff(getFixitureFile('file1.json'), getFixitureFile('file1.json'));
-
-  expect(diff).toEqual(`{
+  const expected = `{
     host: hexlet.io
     timeout: 50
     proxy: 123.234.53.22
     follow: false
-}`);
+}`;
+
+  const diff = genDiff(getFixitureFile('file1.json'), getFixitureFile('file1.json'));
+  
+  expect(diff).toEqual(expected);
 });
 
 test('compare file2.json file2.json ', () => {
-  const diff = genDiff(getFixitureFile('file2.json'), getFixitureFile('file2.json'));
-
-  expect(diff).toEqual(`{
+  const expected = `{
     timeout: 20
     verbose: true
     host: hexlet.io
-}`);
+}`;
+
+  const diff = genDiff(getFixitureFile('file2.json'), getFixitureFile('file2.json'));
+  
+  expect(diff).toEqual(expected);
 });
 
 test('compare file1.json file3.json ', () => {
-  const diff = genDiff(getFixitureFile('file1.json'), getFixitureFile('file3.json'));
 
-  expect(diff).toEqual(`{
+  const expected = `{
     follow: false
   + locathion: hexlet.io
   + schema: https
@@ -50,13 +56,15 @@ test('compare file1.json file3.json ', () => {
   - host: hexlet.io
   - timeout: 50
   - proxy: 123.234.53.22
-}`);
+}`;
+
+  const diff = genDiff(getFixitureFile('file1.json'), getFixitureFile('file3.json'));
+
+  expect(diff).toEqual(expected);
 });
 
 test('compare file2.json file3.json ', () => {
-  const diff = genDiff(getFixitureFile('file2.json'), getFixitureFile('file3.json'));
-
-  expect(diff).toEqual(`{
+  const expected = `{
   + locathion: hexlet.io
   + schema: https
   + protocol: http
@@ -64,18 +72,23 @@ test('compare file2.json file3.json ', () => {
   - timeout: 20
   - verbose: true
   - host: hexlet.io
-}`);
+}`;
+
+  const diff = genDiff(getFixitureFile('file2.json'), getFixitureFile('file3.json'));
+
+  expect(diff).toEqual(expected);
 });
 
-
 test('compare file3.json file3.json ', () => {
-    const diff = genDiff(getFixitureFile('file3.json'), getFixitureFile('file3.json'));
-  
-    expect(diff).toEqual(`{
+
+  const expected = `{
     locathion: hexlet.io
     schema: https
     protocol: http
     follow: false
-}`);
-  });
+}`;
+    const diff = genDiff(getFixitureFile('file3.json'), getFixitureFile('file3.json'));
+  
+    expect(diff).toEqual(expected);
+});
 
---
+++ __tests__/gendiff-yaml.test.js
@@ -0,0 +1,94 @@
+/* eslint-disable */
+
+import path from 'path';
+import genDiff from '../src/gendiff.js';
+
+const getFixitureFile = (fileName) => path.join('__tests__/__fixtures__/', fileName);
+
+test('compare file1.yml file2.yml', () => {
+
+  const expected = `{
+    host: hexlet.io
+  + verbose: true
+  - proxy: 123.234.53.22
+  - follow: false
+  - timeout: 50
+  + timeout: 20
+}`
+
+  const diff = genDiff(getFixitureFile('file1.yml'), getFixitureFile('file2.yml'));
+  
+  expect(diff).toEqual(expected);
+});
+
+test('compare file1.yml file1.yml ', () => {
+  const expected = `{
+    host: hexlet.io
+    timeout: 50
+    proxy: 123.234.53.22
+    follow: false
+}`;
+
+  const diff = genDiff(getFixitureFile('file1.yml'), getFixitureFile('file1.yml'));
+  
+  expect(diff).toEqual(expected);
+});
+
+test('compare file2.yml file2.yml ', () => {
+  const expected = `{
+    timeout: 20
+    verbose: true
+    host: hexlet.io
+}`;
+
+  const diff = genDiff(getFixitureFile('file2.yml'), getFixitureFile('file2.yml'));
+  
+  expect(diff).toEqual(expected);
+});
+
+test('compare file1.yml file3.yml ', () => {
+
+  const expected = `{
+    follow: false
+  + locathion: hexlet.io
+  + schema: https
+  + protocol: http
+  - host: hexlet.io
+  - timeout: 50
+  - proxy: 123.234.53.22
+}`;
+
+  const diff = genDiff(getFixitureFile('file1.yml'), getFixitureFile('file3.yml'));
+
+  expect(diff).toEqual(expected);
+});
+
+test('compare file2.yml file3.yml ', () => {
+  const expected = `{
+  + locathion: hexlet.io
+  + schema: https
+  + protocol: http
+  + follow: false
+  - timeout: 20
+  - verbose: true
+  - host: hexlet.io
+}`;
+
+  const diff = genDiff(getFixitureFile('file2.yml'), getFixitureFile('file3.yml'));
+
+  expect(diff).toEqual(expected);
+});
+
+test('compare file3.yml file3.yml ', () => {
+
+  const expected = `{
+    locathion: hexlet.io
+    schema: https
+    protocol: http
+    follow: false
+}`;
+    const diff = genDiff(getFixitureFile('file3.yml'), getFixitureFile('file3.yml'));
+  
+    expect(diff).toEqual(expected);
+});
+
