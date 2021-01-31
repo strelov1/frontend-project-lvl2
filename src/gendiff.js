56536883aeb64411a18195b975995d7df61fb8f8
strelov1 <strelov1@gmail.com>
2021-01-31 23:12:22 +0300

    took out formater

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import parse from './parsers.js';
+import stylish from './formater.js';
 
 /**
  * Функция сравнивает два обекта и возвращат результат в виде объекта
@@ -40,55 +41,6 @@ function compareObject(obj1, obj2) {
   });
 }
 
-const objToString = (obj) => {
-  const result = Object.entries(obj).map(([key, value]) => `      ${key}: ${value}`);
-  return ['{', ...result, '    }'].join('\n');
-};
-
-/**
- * Функция превращает объект сравнения в строку
- * @param { remain: {}, deleted: {}, added: {}, changed: [] } diffObject
- * @returns { string }
- */
-function diffToString(diffObject) {
-  const extractDiffValues = (diffValues) => Object.entries(diffValues)
-    .map(([key, value]) => {
-      if (typeof value === 'object') {
-        return [key, objToString(value)];
-      }
-      return [key, value];
-    })
-    .map(([key, value]) => `${key}: ${value}`);
-
-  const remain = extractDiffValues(diffObject.remain)
-    .map((item) => `  ${item}`);
-
-  const added = extractDiffValues(diffObject.added)
-    .map((item) => `+ ${item}`);
-
-  const deleted = extractDiffValues(diffObject.deleted)
-    .map((item) => `- ${item}`);
-
-  const changed = Object.entries(diffObject.changed)
-    .map(([key, [val1, val2]]) => {
-      if (typeof val1 === 'object' || typeof val2 === 'object') {
-        return `${key}: ${diffToString(val1)}`.split('\n')
-          .map((item) => `  ${item}`).join('\n');
-      }
-      return [
-        `- ${key}: ${val1}`,
-        `+ ${key}: ${val2}`,
-      ];
-    }).flat();
-
-  const contain = [...remain, ...added, ...deleted, ...changed]
-    .map((item) => `  ${item}`);
-
-  const result = ['{', ...contain, '}'];
-
-  return result.join('\n');
-}
-
 export default function genDiff(filepath1, filepath2) {
   const file1 = fs.readFileSync(path.resolve(filepath1), 'utf8');
   const file2 = fs.readFileSync(path.resolve(filepath2), 'utf8');
@@ -97,5 +49,5 @@ export default function genDiff(filepath1, filepath2) {
   const obj2 = parse(file2, path.extname(filepath2));
 
   const diff = compareObject(obj1, obj2);
-  return diffToString(diff);
+  return stylish(diff);
 }
